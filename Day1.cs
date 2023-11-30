c0dd9bc416ced851037fa59b9bf66fcf9cc426bb
Matthijs de Heus <matthijs.heus@gmail.com>
2023-11-30 19:59:36 +0100

    Add project files.

---
+++ Day1.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC_2023;
+
+public class Day1
+{
+    public static void Main()
+    {
+        List<string> input = Utils.GetInput(1, false);
+
+        Part1(input);
+        //Part2(input);
+
+        _ = Console.ReadKey();
+    }
+
+    public static void Part1(List<string> input)
+    {
+        foreach (string s in input)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    public static void Part2(List<string> input)
+    {
+
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC_2023
+{
+    public static class Utils
+    {
+        public static List<string> GetInput(int day, bool test)
+        {
+            string[] textFile = test == false ? File.ReadAllLines($"../../../Input/{day}.txt") : File.ReadAllLines($"../../../Input/{day}.txt");
+            List<string> input = new(textFile);
+
+            return input;
+        }
+    }
+}
